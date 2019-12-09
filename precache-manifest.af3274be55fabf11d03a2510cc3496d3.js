62e5e87ed8685de0932af5cf034dbad47cf202d2
Thomas Pagram <tpagram@gmail.com>
2019-12-09 19:03:58 +1100

    Updates

---
+++ precache-manifest.af3274be55fabf11d03a2510cc3496d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376daa6fa41b6b7998f1e510e8a4975d",
+    "url": "/logic-detective/index.html"
+  },
+  {
+    "revision": "8b517bc027933f128db7",
+    "url": "/logic-detective/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59d68870f64f625928a0",
+    "url": "/logic-detective/static/js/2.f70a23bd.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/logic-detective/static/js/2.f70a23bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b517bc027933f128db7",
+    "url": "/logic-detective/static/js/main.1d8c024f.chunk.js"
+  },
+  {
+    "revision": "8ff36377aa5be53c7fbc",
+    "url": "/logic-detective/static/js/runtime-main.958dd90d.js"
+  }
+]);
